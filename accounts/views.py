8911be5f3f931848b952b493836a9e4a84b047fc
razzledazzle201 <70910872+razzledazzle201@users.noreply.github.com>
2020-12-05 11:11:20 -0700

    commit on 'master'

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,44 @@
 from django.shortcuts import render
+from django.urls import path
+from . import views
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import NewUserForm
 
 # Create your views here.
 def testview(request):
     print('this is a test.')
     return('the test worked')
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("main:homepage")
+
+def login_request(request):
+    form = AuthenticationForm()
+    return render(request = request,
+                  template_name = "main/login.html",
+                  context={"form":form})
+
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('/')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request = request,
+                    template_name = "main/login.html",
+                    context={"form":form})
 
